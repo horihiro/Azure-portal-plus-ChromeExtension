f3104f63f8a7740a34dc0f7f6ba37964a1196db5
horihiro <hori.kawa.shima.tool+github@gmail.com>
2024-03-27 15:43:52 +0900

    Add new features and fix bugs

--- popup/js/main.js
+++ popup/js/main.js
@@ -1,13 +1,16 @@
 document.addEventListener('DOMContentLoaded', async (e) => {
-  const { replaceFavicon, blinkFavicon, desktopNotification } = (await chrome.storage.local.get(['replaceFavicon', 'blinkFavicon', 'desktopNotification']));
+  const { replaceFavicon, blinkFavicon, desktopNotification, activateTab } = (await chrome.storage.local.get(['replaceFavicon', 'blinkFavicon', 'desktopNotification', 'activateTab']));
 
   const enableReplaceFaviconCheckbox = document.querySelector('#enableReplaceFaviconCheckbox');
   const enableBlinkFaviconCheckbox = document.querySelector('#enableBlinkFaviconCheckbox');
   const enableDesktopNotificationCheckbox = document.querySelector('#enableDesktopNotificationCheckbox');
+  const enableActivateTabCheckbox = document.querySelector('#enableActivateTabCheckbox');
 
   enableReplaceFaviconCheckbox.checked = replaceFavicon && replaceFavicon.status;
   enableBlinkFaviconCheckbox.checked = blinkFavicon && blinkFavicon.status;
   enableDesktopNotificationCheckbox.checked = desktopNotification && desktopNotification.status;
+  enableActivateTabCheckbox.checked = activateTab && activateTab.status;
+
   enableReplaceFaviconCheckbox.addEventListener('change', async (e) => {
     await chrome.storage.local.set({
       'replaceFavicon': {
@@ -29,6 +32,13 @@ document.addEventListener('DOMContentLoaded', async (e) => {
     }
     await chrome.storage.local.set({
       'desktopNotification': {
+        status: e.target.checked
+      }
+    });
+  });
+  enableActivateTabCheckbox.addEventListener('change', async (e) => {
+    await chrome.storage.local.set({
+      'activateTab': {
         status: e.target.checked
       }
     });
